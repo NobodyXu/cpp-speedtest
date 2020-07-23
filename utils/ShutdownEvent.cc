edbca891e6f2f8acea8f1aa95906c3c4e45860c6
nobodyxu <Jiahao_XU@outlook.com>
2020-07-23 13:52:32 +1000

    Impl CtrlCShutdownEvent ctor

--- utils/ShutdownEvent.cc
+++ utils/ShutdownEvent.cc
@@ -1,9 +1,19 @@
 #include "ShutdownEvent.hpp"
 
+#include "sigaction.hpp"
+
 namespace speedtest::utils {
 bool FakeShutdownEvent::has_event() const noexcept
 {
     return false;
+}
+
+CtrlCShutdownEvent::CtrlCShutdownEvent() noexcept
+{
+    sigaction(SIGINT, [](int signum) noexcept
+    {
+        CtrlCShutdownEvent::is_triggered = true;
+    });
 }
 bool CtrlCShutdownEvent::has_event() const noexcept
 {
