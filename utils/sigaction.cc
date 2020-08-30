d9bed56c9b2e9380450d90bafed114d922719a8b
nobodyxu <Jiahao_XU@outlook.com>
2020-08-30 22:09:00 +1000

    Fix us of struct sigaction in utils::sigaction

--- utils/sigaction.cc
+++ utils/sigaction.cc
@@ -1,11 +1,14 @@
 #include "sigaction.hpp"
 
+#include <string.h>
 #include <err.h>
 
 namespace speedtest::utils {
 void sigaction(int signum, void (*handler)(int)) noexcept
 {
     struct sigaction act;
+    memset (&act, 0, sizeof (act));
+
     act.sa_handler = handler;
     if (sigaction(SIGABRT, &act, nullptr) == -1)
         err(1, "Attempt to register %d with void (*)(int) handler %p failed", signum, handler);
